6f9c84f01d905172a5fe2becb5872ae8b7046b96
Diego Quintana <daquintanav@gmail.com>
2020-08-10 18:24:33 +0200

    refactor entrypoint

--- python-project/project/__init__.py
+++ python-project/project/__init__.py
@@ -1,18 +1,20 @@
+import logging
+
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.engine.base import Engine
 
+logging.basicConfig(level=logging.INFO)
 
 # from project.models.classic import aims_meta, amos_meta
 # from project.models.declarative import AIMSBase, AMOSBase
 from project.base.config import BaseConfig
 from project.scripts.db_utils import create_all, delete_all, get_session
-from project.providers import fake
-
+from project.scripts.generate import AircraftGenerator
 
 if __name__ == "__main__":
 
-    config = BaseConfig()
-    engine = create_engine(config.db_url, echo=True)
+    config = BaseConfig(size=10)
+    engine = create_engine(config.db_url, echo=False)
 
     # create session
     delete_all(engine)
@@ -33,10 +35,10 @@ where
     for _r in result:
         print(_r)
 
-    # attempt to create an instance using the classical mappings
-    fs = fake.flight_slot()
-
     session = get_session(engine)
+    ag = AircraftGenerator(config)
+    ag.populate()
+    ag.to_sql(session)
 
-    session.add(fs)
-    session.commit()
+    delete_all(engine)
+
