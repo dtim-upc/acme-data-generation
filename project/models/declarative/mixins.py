972845941831256147f53f97228d69b04910bb90
Diego Quintana <daquintanav@gmail.com>
2020-11-03 01:43:54 +0100

    fix the utils mixin passing a "type" column silently

--- project/models/declarative/mixins.py
+++ project/models/declarative/mixins.py
@@ -5,16 +5,15 @@ from sqlalchemy import inspect
 class UtilsMixin(object):
     """A mixin to implement a generic utilities"""
 
-    def __repr__(self):        
-        inspector = inspect(self.__class__)
+    def __repr__(self):
         return "%s(%s)" % (
             self.__class__.__name__,
-            ", ".join(
-                [f"{c}={getattr(self, c)}" for c in inspector.attrs.keys()]
-            ),
+            ", ".join([f"{k}={v}" for k, v in self.as_dict().items()]),
         )
 
     def as_dict(self):
         """return instance as a dictionary"""
         inspector = inspect(self.__class__)
-        return {c: getattr(self, c) for c in inspector.attrs.keys() if not c.startswith("_")}
+        return {
+            c: getattr(self, c) for c in inspector.attrs.keys() if c not in {"type", "_sa_polymorphic_on"}
+        }
